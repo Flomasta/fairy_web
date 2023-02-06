23fdd339692e76d5a5925285c67478eed09d619d
flomasta <zatevi@mail.ru>
2023-02-06 23:25:58 +0300

    Create chat messages in the room

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.http import HttpResponse
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib import messages
@@ -73,15 +73,21 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+    context = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/room.html', context)
 
 
 @login_required(login_url='login_page')
 def createRoom(request):
     form = RoomForm()
-    if request.user != room.host or request.user:
-        return HttpResponse('You are not allowed here')
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
