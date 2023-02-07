309600755d4adc12ec8a3cf49c70351c9b493baa
flomasta <zatevi@mail.ru>
2023-02-07 21:55:19 +0300

    Simle restApi exchange realized

--- base/urls.py
+++ base/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -15,5 +15,6 @@ urlpatterns = [
     path('update-user/', views.updateUser, name='update-user'),
     path('topics/', views.topicsPage, name='topics'),
     path('activity/', views.activityPage, name='activity'),
+    path('api/', include('base.api.urls')),
 
 ]
