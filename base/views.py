3c214822dc1e40087e0a10c258a1499b3d300cd0
flomasta <zatevi@mail.ru>
2023-02-07 19:57:01 +0300

    Mobile version for recent activities created

--- base/views.py
+++ base/views.py
@@ -59,7 +59,7 @@ def registerPage(request):
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
@@ -179,3 +179,9 @@ def topicsPage(request):
     topics = Topic.objects.filter(name__icontains=q)
     context = {'topics': topics}
     return render(request, 'base/topics.html', context)
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()[0:5]
+    context = {'room_messages': room_messages}
+    return render(request, 'base/activity.html', context)
